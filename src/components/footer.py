aef2a0f09d1bd9ce898153914450d0e266692090
mockingbird9298@gmail.con <subhajitsau604373@gmail.com>
2026-08-20 03:11:07 +0530

    Changing footer

--- src/components/footer.py
+++ src/components/footer.py
@@ -6,10 +6,7 @@ def footer_home():
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        # <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
-        # <img src='{logo_url}' style='max-height:25px' />
-
-        <p> Created by Subhajit <p/>
+      
         </div>
                 
                 """, unsafe_allow_html=True)
@@ -20,9 +17,7 @@ def footer_dashboard():
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        # <p style="font-weight:bold; color:black;"> Created with ❤️ by </p>  
-        # <img src='{logo_url}' style='max-height:25px' />
-        <p> Created by Subhajit <p/>
+      
 
         </div>
                 
